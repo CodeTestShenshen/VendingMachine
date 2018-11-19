6d55a7a25acf5a65588fd8dec1005e0b41d945f2
sm375GitHub <shenshen.github@gmail.com>
2018-11-19 15:00:34 +1100

    feat: UI coin calculator layout

--- ax/src/app/calculator/input-panel/input-button/input-button.component.ts
+++ ax/src/app/calculator/input-panel/input-button/input-button.component.ts
@@ -7,18 +7,42 @@ import { Cash } from '../../../shared/running-state.model';
   styleUrls: ['./input-button.component.scss']
 })
 export class InputButtonComponent implements OnInit {
-  @Input() cash: Cash = new Cash();
+  @Input() cash: Cash;
   @Input() disabled = false;
-  @Output() moneySelect: EventEmitter<number> = new EventEmitter<number>();
+  @Output() insertCash: EventEmitter<Cash> = new EventEmitter<Cash>();
   constructor() { }
   onMoneySelect() {
-    this.moneySelect.emit(this.cash.unit);
+    this.insertCash.emit(this.cash);
   }
 
   isEnabled(): boolean {
     return (this.cash.quantity > 0 && !this.disabled);
   }
   ngOnInit() {
+  }
+
+  getCashDispayValue(): string {
+    if (this.cash) {
+      return '$' + (this.cash.unit / 100).toFixed(2);
+    }
+    return '$0.00';
+  }
+
+  getButtonClass(): string {
+    if (this.cash) {
+      return this.cash.quantity > 0 ? 'btn btn-primary' : 'btn btn-secondary';
+    }
+    return 'btn btn-secondary';
+  }
+
+  IsAvaible(): boolean {
+    return this.isEnabled && this.cash.quantity > 0;
+  }
+
+  onInsertCash() {
+    this.insertCash.emit(this.cash);
+    console.log('cash is inserted: ');
+    console.log(this.cash);
   }
 
 }
